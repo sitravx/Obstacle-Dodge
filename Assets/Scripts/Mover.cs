ff9f1929e6c2a1fb389888382417f20bc7f956cf
ılgın <139047183+sitravx@users.noreply.github.com>
2025-10-03 14:18:05 +0300

    ObstacleDodge-V1.2
    coding about some variables for learn

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    
+    float xValue = 0.01f;
+    float yValue = 0.00f;
+    float zValue = 0.00f;
     void Start()
     {
         
@@ -11,6 +13,6 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
