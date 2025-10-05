f75bea19415e0b9c2dc726101141405769075ae0
ılgın <139047183+sitravx@users.noreply.github.com>
2025-10-05 15:57:43 +0300

    ObstacleDodge-V1.8
    player following orbs

---
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class FlyAtPlayer : MonoBehaviour
+{
+
+
+    [SerializeField] private float speed = 2f;
+    [SerializeField] Transform player;
+    
+    void Update()
+    {
+        MoveToPlayer();
+        DestryoWhenReached();
+    }
+
+    void MoveToPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+    void DestryoWhenReached()
+    {
+        if (transform.position == player.transform.position)
+        {
+            if (transform.position == player.transform.position)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
