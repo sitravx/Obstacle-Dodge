0c3c9ba03c322daed5f9e1d1d410c60e7aac293c
ılgın <139047183+sitravx@users.noreply.github.com>
2025-10-05 13:04:23 +0300

    ObstacleDodge-V1.6.1
    Update Dropper

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] private float waitTime = 3f;
+
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
     void Update()
     {
         Debug.Log(Time.time);
-        if(Time.time > 3f)
+        
+        if (Time.time > waitTime)
         {
+            Debug.Log("Gravity on!");
+            GetComponent<MeshRenderer>().enabled = true;
             GetComponent<Rigidbody>().useGravity = true;
         }
     }
